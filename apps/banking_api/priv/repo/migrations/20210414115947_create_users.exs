00feac7f067bebe99f8a2a9f7158ca956e07cd44
Franciele Portugal <franciele.santos@stone.com.br>
2021-04-14 10:24:57 -0300

    feat: create users migration

---
+++ apps/banking_api/priv/repo/migrations/20210414115947_create_users.exs
@@ -0,0 +1,11 @@
+defmodule BankingApi.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :name, :string
+      add :email, :string
+    end
+  end
+end

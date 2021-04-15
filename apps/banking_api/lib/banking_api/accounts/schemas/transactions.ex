6fbd51bee7899a37ad50e94d9a1ab0210caf7aff
Franciele Portugal <franciele.santos@stone.com.br>
2021-04-15 10:58:06 -0300

    fix: rename transfer scheme to transactions

--- apps/banking_api/lib/banking_api/accounts/schemas/transactions.ex
+++ apps/banking_api/lib/banking_api/accounts/schemas/transactions.ex
@@ -1,16 +1,18 @@
-defmodule BankingApi.Accounts.Schemas.Transfers do
+defmodule BankingApi.Accounts.Schemas.Transactions do
   use Ecto.Schema
   import Ecto.Changeset
 
   alias BankingApi.Accounts.Schemas.Account
+  alias BankingApi.Accounts.Schemas.Operation
 
   @required [:value]
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
-  schema "transfers" do
+  schema "transactions" do
     field :value, :integer
 
+    belongs_to :operation, Operation
     belongs_to :origin_account, Account
     belongs_to :destination_account, Account
 
